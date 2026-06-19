923df6b17a245a61a4a8965e20fd00d47fabb539
Sara Pajic <sarapajic98@gmail.com>
2026-06-19 17:34:14 +0200

    create and get Booking tests added

--- src/test/java/tests/BookingTests.java
+++ src/test/java/tests/BookingTests.java
@@ -7,6 +7,46 @@ import static io.restassured.RestAssured.given;
 
 public class BookingTests extends BaseTest {
 
+    static int bookingId;
+
+    @Test
+    void createBooking() {
+        String body = """
+    {
+        "firstname": "Sara",
+        "lastname": "Test",
+        "totalprice": 120,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2026-07-07",
+            "checkout": "2025-08-08"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    """;
+
+        bookingId =
+                given()
+                        .contentType("application/json")
+                        .body(body)
+                        .when()
+                        .post("/booking")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("bookingid");
+    }
+
+    @Test
+    void getBookingWithId() {
+        given()
+                .contentType("application/json")
+                .when()
+                .get("/booking/" + bookingId)
+                .then()
+                .statusCode(200);
+    }
+
     @Test
     void getBooking() {
         given()
