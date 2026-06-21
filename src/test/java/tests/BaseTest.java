519b58ba08a1426064885d10dac70d31016eefc9
Sara Pajic <sarapajic98@gmail.com>
2026-06-21 19:14:53 +0200

    CRUD booking tests with dynamic test data added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,9 +2,12 @@ package tests;
 
 import config.RequestSpecificationFactory;
 import io.restassured.specification.RequestSpecification;
+import services.BookingService;
 
 public class BaseTest {
 
-    protected RequestSpecification requestSpec =
+    protected static final RequestSpecification requestSpec =
             RequestSpecificationFactory.getRequestSpecification();
+
+    protected static final BookingService service = new BookingService();
 }
--- src/test/java/tests/BookingTests.java
+++ src/test/java/tests/BookingTests.java
@@ -2,16 +2,120 @@ package tests;
 
 import base.BaseTest;
 import helper.Booking;
-import helper.BookingDates;
 import org.junit.jupiter.api.Test;
 import services.BookingService;
 import testdata.BookingDataFactory;
-import utils.AuthTokenManager;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static tests.BaseTest.requestSpec;
+import static tests.BaseTest.service;
 
 public class BookingTests extends BaseTest {
+
+    @Test
+    void createBookingTest() {
+
+        Booking booking = BookingDataFactory.createRandomBooking();
+
+        int id = service.createBooking(requestSpec, booking);
+
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/booking/" + id)
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo(booking.getFirstname()))
+                .body("lastname", equalTo(booking.getLastname()));
+    }
+
+    @Test
+    void updateBookingTest() {
+
+        Booking booking = BookingDataFactory.createRandomBooking();
+        int id = service.createBooking(requestSpec, booking);
+
+        Booking updated = new Booking(
+                booking.getFirstname(),
+                booking.getLastname(),
+                booking.getTotalprice() + 50,
+                booking.isDepositpaid(),
+                booking.getBookingdates(),
+                "PARKING"
+        );
+
+        service.updateBooking(requestSpec, id, updated)
+                .then()
+                .statusCode(200)
+                .body("totalprice", equalTo(booking.getTotalprice() + 50));
+    }
+
+    @Test
+    void partialUpdateValidationTest() {
+
+        Booking booking = BookingDataFactory.createRandomBooking();
+        int id = service.createBooking(requestSpec, booking);
+
+        Booking updated = new Booking(
+                booking.getFirstname(),
+                booking.getLastname(),
+                booking.getTotalprice(),
+                booking.isDepositpaid(),
+                booking.getBookingdates(),
+                "BREAKFAST"
+        );
+
+        service.updateBooking(requestSpec, id, updated)
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo(booking.getFirstname()))
+                .body("additionalneeds", equalTo("BREAKFAST"));
+    }
+
+    @Test
+    void deleteBookingTest() {
+
+        Booking booking = BookingDataFactory.createRandomBooking();
+        int id = service.createBooking(requestSpec, booking);
+
+        service.deleteBooking(requestSpec, id)
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    void verifyDeleteBookingTest() {
+
+        Booking booking = BookingDataFactory.createRandomBooking();
+        int id = service.createBooking(requestSpec, booking);
+
+        service.deleteBooking(requestSpec, id);
+
+        service.getBooking(requestSpec, id)
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void createBookingNegativeTest() {
+
+        String invalidBody = """
+        {
+            "firstname": "",
+            "lastname": "",
+            "totalprice": "abc"
+        }
+    """;
+
+        given()
+                .spec(requestSpec)
+                .body(invalidBody)
+                .when()
+                .post("/booking")
+                .then()
+                .statusCode(500);
+    }
 
     @Test
     void fullBookingFlow() {
